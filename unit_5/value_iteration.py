4a9d1b1e265c00b380e43ea619f6799b944ca641
marino.sanlorenzo.ext <marino.sanlorenzo.ext@vodeno.com>
2020-04-23 13:38:52 +0200

    RL value iteration

--- unit_5/value_iteration.py
+++ unit_5/value_iteration.py
@@ -27,14 +27,28 @@ class State1D:
 
 class Reward:
 
-	def __init__(self, coord, reward):
-		validate_type(coord,int)
+	def __init__(self, coord_ini, action, coord_final, reward):
+		validate_type(coord_ini,int)
+		validate_type(action, tuple)
+		validate_type(coord_final, int)
 		validate_type(reward, Real)
-		self.coord = coord
+		self.coord_ini = coord_ini
+		self.action = action
+		self.coord_final = coord_final
 		self.reward = reward
 
+	def __repr__(self):
+		return f'R(s={self.coord_ini}, a={self.action}, sf={self.coord_final})={self.reward}'
+
+	@property
+	def reward(self):
+		return self.reward
+		:
 
 
+# =============================================================================
+# VARIABLES
+# =============================================================================
 
 nb_states = 5
 states = [State1D(state) for state in range(nb_states)]
