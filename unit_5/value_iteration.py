a5cfca2b43bd7fa856dda1be1b6d1e224861a36b
marino.sanlorenzo.ext <marino.sanlorenzo.ext@vodeno.com>
2020-04-26 11:17:05 +0200

    update

--- unit_5/value_iteration.py
+++ unit_5/value_iteration.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+import pandas as pd
 import numpy as np
 from numbers import Real
 from recordtype import recordtype
@@ -78,14 +79,14 @@ class TransitionProb:
 			[1 / 4, 1 / 2, 1 / 4, 0, 0],
 			[0, 1 / 4, 1 / 2, 1 / 4, 0],
 			[0, 0, 1 / 4, 1 / 2, 1 / 4],
-			[0, 0, 0, 1 / 2, 1 / 2]
+			[0, 0, 0, 0 , 1]
 
 		]), 'LEFT': np.array([
-			[1, 0, 0, 0, 0],
+			[1/2, 1/2, 0, 0, 0],
 			[1 / 3, 2 / 3, 0, 0, 0],
 			[0, 1 / 3, 2 / 3, 0, 0],
 			[0, 0, 1 / 3, 2 / 3, 0],
-			[0, 0, 0, 1 / 3, 2 / 3]
+			[0, 0, 0, 0, 1]
 
 		]), 'RIGHT': np.array([
 			[2 / 3, 1 / 3, 0, 0, 0],
@@ -107,24 +108,25 @@ class ValueNotInCache(KeyError):
 
 class Value:
 
-	def __init__(self, states, rewards, actions, trans_proba, discount = 0.5):
+	def __init__(self, states, rewards_matrix, actions, trans_proba, discount = 0.5):
 		self.discount = discount
 		self.states = states
-		self.rewards = rewards
-		self.actions = actions
+		self.rewards = rewards_matrix
+		self.actions_list = actions.get_list_actions()
 		self.trans_proba = trans_proba.transitions_probas
-		self.value_dic = {}
+		self.value_star_dic = {}
+		self.action_star_dic = {}
 		self.value = np.array([0 for _ in range(len(self.rewards))])
-		self.value_dic[0] = self.value
+		self.value_star_dic[0] = self.value
 
 	def __repr__(self):
-		return f'{self.value_dic}'
+		return f'{self.value_star_dic}'
 
 	def check_cache(self,k):
 		try:
-			a = self.value_dic[k]
+			a = self.value_star_dic[k]
 		except KeyError:
-			msg = f'Key:{k} is not in {self.value_dic}'
+			msg = f'Key:{k} is not in {self.value_star_dic}'
 			raise ValueNotInCache(msg)
 
 
@@ -133,32 +135,99 @@ class Value:
 
 	def get_value_k_for_state_and_action(self, state, action , k):
 		self.check_cache(k-1)
-		self.value_k_1 = self.value_dic[k-1]
-		return np.sum(self.get_trans_proba(action, state)*(self.rewards[state]() + self.discount*self.value_k_1))
+		self.value_k_1 = self.value_star_dic[k-1]
+		rewards = self.rewards[action][state]
+		print(rewards)
+		print(rewards.shape)
+		print(self.get_trans_proba(action, state).shape)
+		print(self.discount*self.value_k_1.shape)
+		# if state == 4 and (k-1) >= 1:
+		# 	return self.value_k_1[state]
+		return np.sum(self.get_trans_proba(action, state)*(rewards + self.discount*self.value_k_1))
 
 	def get_value_k_for_all_states_for_action(self, action, k):
 		return [ self.get_value_k_for_state_and_action(state=state(),
 																action = action,
 																k=k) for state in self.states]
 
+	def get_value_k_for_all_actions(self,k):
+		value_k = {}
+		for action in self.actions_list:
+			value_k['k']= k
+			value_k[action] = self.get_value_k_for_all_states_for_action( action = action, k = k)
+		return value_k
 
+	def get_value_star_k(self, k):
+		df = pd.DataFrame(self.get_value_k_for_all_actions(k))
+		df_actions = df[self.actions_list]
+		value_star_k = df_actions.values.max(axis=1)
+		self.value_star_dic[k] = value_star_k
+		self.action_star_dic[k] = df_actions.idxmax(axis =1)
+
+	def print_info(self, last_row = True):
+		df_action_star = pd.DataFrame.from_dict(self.action_star_dic, orient = 'index')
+		df_value_star = pd.DataFrame.from_dict(self.value_star_dic, orient = 'index')
+		if last_row:
+			last_idx = df_value_star.last_valid_index()
+			last_value = list(df_value_star.iloc[last_idx, ::])
+			last_act_idx = df_action_star.last_valid_index()
+			last_action = list(df_action_star.iloc[last_act_idx-1, ::])
+			print(f'last value stars:\n{last_value}')
+			print('-----------------------------------------------------')
+			print(f'last action stars:\n{last_action}')
+
+	def get_actions_df(self):
+		return pd.DataFrame.from_dict(self.action_star_dic, orient = "index")
+
+	def get_values_df(self):
+		return pd.DataFrame.from_dict(self.value_star_dic, orient = "index")
+
+	def get_final_output(self):
+		df = self.get_values_df()
+		last_output = list(df.iloc[df.last_valid_index(),::])
+		return [round(e,4) for e in last_output]
 # =============================================================================
 # VARIABLES
 # =============================================================================
 
-def test():
+def run():
+	import sys
+	print(sys.argv)
+	RANGE = int(sys.argv[1])
 	nb_states = 5
 	states = [State1D(state) for state in range(nb_states)]
-	rewards_array = np.array([0,0,0,0,1])
-	rewards = [Reward(i,'R', i+1, reward) for i,reward in zip(range(len(states)), rewards_array)  ]
+	rewards_matrix = {'STAY': np.array([[0,0,0,0,0],
+							 [0,0,0,0,0],
+							 [0,0,0,0,0],
+							 [0,0,0,1,0],
+							 [0,0,0,0,1]]),
+					  'RIGHT':np.array([[0, 0, 0, 0, 0],
+								[0, 0, 0, 0, 0],
+								[0, 0, 0, 0, 0],
+								[0, 0, 0, 0, 1],
+								[0, 0, 0, 0, 0]]),
+					 'LEFT':np.array([[0, 0, 0, 0, 0],
+							   [0, 0, 0, 0, 0],
+							   [0, 0, 0, 0, 0],
+							   [0, 0, 0, 0, 0],
+							   [0, 0, 0, 0, 1]])}
+
+	# rewards = []
+	# for i in range(rewards_array.shape[0]):
+	# 	for j in range(rewards_array.shape[1]):
+	# 		rewards.append(Reward(i,'ALL', j, reward=rewards_array[i,j]))
+
 	actions = Action()
 	trans_proba = TransitionProb()
-	discount = 1/2
-	v = Value(states, rewards, actions, trans_proba)
-	v.get_value_k_for_state_and_action(state=0,action='STAY',k=1)
+	v = Value(states, rewards_matrix, actions, trans_proba)
 
-	assert np.all(np.array([0,0,0,0,1]) ==
-				  v.get_value_k_for_all_states_for_action(action = 'STAY', k=1))
+	for k in range(1,RANGE):
+		v.get_value_star_k(k)
+		v.print_info()
+	else:
+		print('You reached convergence and finished the exploration mode!')
+		print(v.get_final_output())
 
+		#[0.016667,0.05,0.2,0.8,1.2]
 if __name__ == '__main__':
-    test()
+    run()
